5d38ccf894f3815335b647c8c70fea6f17aa45d7
hannahsouthwick <hasouthwick.18@gmail.com>
2022-03-01 10:15:36 -0700

    add cart icon functionality
    No showing up yet, navigation works but image and total does not

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BookstoreMission.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreMission.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
