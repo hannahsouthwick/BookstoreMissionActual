4bfe91ce54c5bc504da97c85731313ac96da882d
hannahsouthwick <hasouthwick.18@gmail.com>
2022-03-09 15:01:37 -0700

    bootstrap fixes

--- Migrations/20220309190719_InitialCreate2.cs
+++ Migrations/20220309190719_InitialCreate2.cs
@@ -6,89 +6,88 @@ namespace BookstoreMission.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Books",
-                columns: table => new
-                {
-                    BookID = table.Column<int>(nullable: false),
-                    Title = table.Column<string>(nullable: false),
-                    Author = table.Column<string>(nullable: false),
-                    Publisher = table.Column<string>(nullable: false),
-                    ISBN = table.Column<string>(nullable: false),
-                    Classification = table.Column<string>(nullable: false),
-                    Category = table.Column<string>(nullable: false),
-                    PageCount = table.Column<int>(nullable: false),
-                    Price = table.Column<double>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Books", x => x.BookID);
-                });
+            //    migrationBuilder.CreateTable(
+            //        name: "Books",
+            //        columns: table => new
+            //        {
+            //            BookID = table.Column<int>(nullable: false),
+            //            Title = table.Column<string>(nullable: false),
+            //            Author = table.Column<string>(nullable: false),
+            //            Publisher = table.Column<string>(nullable: false),
+            //            ISBN = table.Column<string>(nullable: false),
+            //            Classification = table.Column<string>(nullable: false),
+            //            Category = table.Column<string>(nullable: false),
+            //            PageCount = table.Column<int>(nullable: false),
+            //            Price = table.Column<double>(nullable: false)
+            //        },
+            //        constraints: table =>
+            //        {
+            //            table.PrimaryKey("PK_Books", x => x.BookID);
+            //        });
 
-            migrationBuilder.CreateTable(
-                name: "Purchases",
-                columns: table => new
-                {
-                    PurchaseId = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Name = table.Column<string>(nullable: false),
-                    AddressLine1 = table.Column<string>(nullable: false),
-                    AddressLine2 = table.Column<string>(nullable: true),
-                    AddressLine3 = table.Column<string>(nullable: true),
-                    City = table.Column<string>(nullable: false),
-                    State = table.Column<string>(nullable: false),
-                    Zip = table.Column<string>(nullable: true),
-                    Country = table.Column<string>(nullable: false),
-                    PurchaseShipped = table.Column<bool>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Purchases", x => x.PurchaseId);
-                });
+            //    migrationBuilder.CreateTable(
+            //        name: "Purchases",
+            //        columns: table => new
+            //        {
+            //            PurchaseId = table.Column<int>(nullable: false)
+            //                .Annotation("Sqlite:Autoincrement", true),
+            //            Name = table.Column<string>(nullable: false),
+            //            AddressLine1 = table.Column<string>(nullable: false),
+            //            AddressLine2 = table.Column<string>(nullable: true),
+            //            AddressLine3 = table.Column<string>(nullable: true),
+            //            City = table.Column<string>(nullable: false),
+            //            State = table.Column<string>(nullable: false),
+            //            Zip = table.Column<string>(nullable: true),
+            //            Country = table.Column<string>(nullable: false)
+            //        },
+            //        constraints: table =>
+            //        {
+            //            table.PrimaryKey("PK_Purchases", x => x.PurchaseId);
+            //        });
 
-            migrationBuilder.CreateTable(
-                name: "CartLineItem",
-                columns: table => new
-                {
-                    LineID = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    BookId = table.Column<int>(nullable: true),
-                    Quantity = table.Column<int>(nullable: false),
-                    Price = table.Column<double>(nullable: false),
-                    PurchaseId = table.Column<int>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CartLineItem", x => x.LineID);
-                    table.ForeignKey(
-                        name: "FK_CartLineItem_Books_BookId",
-                        column: x => x.BookId,
-                        principalTable: "Books",
-                        principalColumn: "BookID",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_CartLineItem_Purchases_PurchaseId",
-                        column: x => x.PurchaseId,
-                        principalTable: "Purchases",
-                        principalColumn: "PurchaseId",
-                        onDelete: ReferentialAction.Restrict);
-                });
+            //    migrationBuilder.CreateTable(
+            //        name: "CartLineItem",
+            //        columns: table => new
+            //        {
+            //            LineID = table.Column<int>(nullable: false)
+            //                .Annotation("Sqlite:Autoincrement", true),
+            //            BookId = table.Column<int>(nullable: true),
+            //            Quantity = table.Column<int>(nullable: false),
+            //            Price = table.Column<double>(nullable: false),
+            //            PurchaseId = table.Column<int>(nullable: true)
+            //        },
+            //        constraints: table =>
+            //        {
+            //            table.PrimaryKey("PK_CartLineItem", x => x.LineID);
+            //            table.ForeignKey(
+            //                name: "FK_CartLineItem_Books_BookId",
+            //                column: x => x.BookId,
+            //                principalTable: "Books",
+            //                principalColumn: "BookID",
+            //                onDelete: ReferentialAction.Restrict);
+            //            table.ForeignKey(
+            //                name: "FK_CartLineItem_Purchases_PurchaseId",
+            //                column: x => x.PurchaseId,
+            //                principalTable: "Purchases",
+            //                principalColumn: "PurchaseId",
+            //                onDelete: ReferentialAction.Restrict);
+            //        });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Books_BookID",
-                table: "Books",
-                column: "BookID",
-                unique: true);
+            //    migrationBuilder.CreateIndex(
+            //        name: "IX_Books_BookID",
+            //        table: "Books",
+            //        column: "BookID",
+            //        unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_CartLineItem_BookId",
-                table: "CartLineItem",
-                column: "BookId");
+            //    migrationBuilder.CreateIndex(
+            //        name: "IX_CartLineItem_BookId",
+            //        table: "CartLineItem",
+            //        column: "BookId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_CartLineItem_PurchaseId",
-                table: "CartLineItem",
-                column: "PurchaseId");
+            //    migrationBuilder.CreateIndex(
+            //        name: "IX_CartLineItem_PurchaseId",
+            //        table: "CartLineItem",
+            //        column: "PurchaseId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
